2db9dbbd0baef5dbf90105738af87189572dc88f
6h4ack <66249467+6h4ack@users.noreply.github.com>
2020-06-02 13:51:00 +0200

    Update Interfaz.py

--- VirusTotal_terminal/Interfaz.py
+++ VirusTotal_terminal/Interfaz.py
@@ -1,23 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    Trabajo Fin de Master 
-    Copyright (C) 2018  Gonzalo Aguilar Garcia
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    
-    e-mail: gonzaloaguil@hotmail.com
-"""
 
 from fpdf import FPDF
 from ipwhois import IPWhois
@@ -521,9 +502,6 @@ def main():
         else:
             print ("")
             input("No has pulsado ninguna opción correcta...\npulsa INTRO para continuar")
-
-
-
 
 
 #Llamamos al la funcion main
